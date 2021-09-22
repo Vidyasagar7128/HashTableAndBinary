3f06dafd0825f43f4319af6aa454b7976a6fc6a2
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-22 22:52:49 +0530

    To be or not to be

--- HashTableAndBinary/Program.cs
+++ HashTableAndBinary/Program.cs
@@ -7,6 +7,17 @@ namespace HashTableAndBinary
         static void Main(string[] args)
         {
             Console.WriteLine("Hash Table!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("Fifth index is: {0} ", hash5);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("Second index is: {0} ", hash2);
         }
     }
 }
